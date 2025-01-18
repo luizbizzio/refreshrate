39746c1cc484924eb089f75b5c0dbf45a19691c8
Luiz Bizzio <luizrbizzio@gmail.com>
2025-01-18 22:59:37 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,56 @@
+const CACHE_NAME = 'cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icon.svg',
+  '/icon.png',
+  '/manifest.webmanifest',
+  '/css/main.css',
+  '/LICENSE',
+  '/robots.txt',
+  '/sw.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const currentCaches = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!currentCaches.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request).then((networkResponse) => {
+        if (
+          !event.request.url.includes('/browser-sync/') &&
+          event.request.method === 'GET'
+        ) {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        }
+        return networkResponse;
+      });
+    })
+  );
+});
